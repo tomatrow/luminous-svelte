20c1c667dbcd14d43f69e425b2a3fa376416da85
AJ Caldwell <tomatrow@gmail.com>
2021-04-01 19:48:17 -0700

    Mess with build config

--- rollup.config.js
+++ rollup.config.js
@@ -4,19 +4,19 @@ import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
-const prod = process.env.NODE_ENV === 'production';
+const production = process.env.NODE_ENV === 'production';
 
 export default {
   input: './src/index.ts',
   external: ['svelte'],
   output: [
-    { file: pkg.browser, format: 'umd', sourcemap: prod, name: "luminous-svelte" },
-    { file: pkg.module, format: 'esm', sourcemap: prod },
+    { file: pkg.browser, format: 'umd', sourcemap: production, name: "LuminousSvelte" },
+    { file: pkg.module, format: 'esm', sourcemap: production },
   ],
   plugins: [
     resolve({ browser: true }),
     commonjs(),
     typescript(),
-    prod && terser(),
+    production && terser(),
   ],
 };
