ae24584b7ad2765228b9d4d64366144bc94b420c
AJ Caldwell <tomatrow@gmail.com>
2021-04-14 01:36:09 -0700

    Some changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
